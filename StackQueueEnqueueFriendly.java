30a3b09aac7a601638268e37f672e68da198e605
kavimadu2006 <madukavindi05.com@gmail.com>
2026-06-20 11:46:32 +0530

    Update StackQueueEnqueueFriendly.java

--- StackQueueEnqueueFriendly.java
+++ StackQueueEnqueueFriendly.java
@@ -1,1 +1,35 @@
+import java.util.Stack;
 
+public class StackQueueEnqueueFriendly {
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    public void enqueue(int value) {
+        s1.push(value);
+    }
+
+    public int dequeue() {
+        if (s1.isEmpty() && s2.isEmpty()) {
+            System.out.println("Queue is Empty");
+            return -1;
+        }
+
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+
+        return s2.pop();
+    }
+
+    public static void main(String[] args) {
+        StackQueueEnqueueFriendly q = new StackQueueEnqueueFriendly();
+
+        q.enqueue(10);
+        q.enqueue(20);
+        q.enqueue(30);
+
+        System.out.println("Dequeued: " + q.dequeue());
+    }
+}
