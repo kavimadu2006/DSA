01e10293b9eec0d2299794dcdddb96bca3c3ac42
kavimadu2006 <madukavindi05.com@gmail.com>
2026-07-28 23:01:49 +0530

    Create OptimizedQuickSort.java

---
+++ Submission6/OptimizedQuickSort.java
@@ -0,0 +1,76 @@
+public class OptimizedQuickSort {
+
+    // Partition Method
+    static int partition(int arr[], int low, int high) {
+
+        // Median Pivot (Middle Element)
+        int mid = low + (high - low) / 2;
+        swap(arr, mid, high);
+
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, i + 1, high);
+
+        return i + 1;
+    }
+
+    static void quickSort(int arr[], int low, int high) {
+
+        while (low < high) {
+
+            int pi = partition(arr, low, high);
+
+            // Recur for smaller partition first
+            if (pi - low < high - pi) {
+
+                quickSort(arr, low, pi - 1);
+                low = pi + 1;
+
+            } else {
+
+                quickSort(arr, pi + 1, high);
+                high = pi - 1;
+            }
+        }
+    }
+
+    static void swap(int arr[], int i, int j) {
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+    }
+
+    static void printArray(int arr[]) {
+
+        for (int num : arr)
+            System.out.print(num + " ");
+
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {35, 12, 43, 8, 51, 24, 17, 99, 1};
+
+        System.out.println("Before Sorting:");
+        printArray(arr);
+
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println("After Sorting:");
+        printArray(arr);
+
+    }
+}
